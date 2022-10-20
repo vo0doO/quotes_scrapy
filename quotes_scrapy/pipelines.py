283e4832bff33d2164ac59b1fcc4977317881b01
vo0doo <kirsanov.bvt@gmail.com>
2022-10-20 13:18:06 +0300

    update pipelines.py

--- quotes_scrapy/pipelines.py
+++ quotes_scrapy/pipelines.py
@@ -5,9 +5,32 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import json
+from scrapy.exceptions import DropItem
 
 
-class Quotes_scrapylPipeline:
+class JsonWriterPipeline:
+
+    def open_spider(self, spider):
+        self.file = open(f"json-writer-pipeline-file-{spider.name}.jsonl", "w")
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(item) + "\n"
+        self.file.write(line)
         return item
+
+
+class DublicatesPipeLine:
+
+    def __init__(self):
+        self.hash_seen = set()
+
+    def process_item(self, item, spider):
+        if item['hash'] in self.hash_seen:
+            raise DropItem(f"Обнаружен  дубликат ==> {item}")
+        else:
+            self.hash_seen.add(item["hash"])
+            return item
