691c39b66b82f40afc9f1c45f3238e33bb14b718
vo0doo <kirsanov.bvt@gmail.com>
2022-10-20 13:18:21 +0300

    update quotes_spider.py

--- quotes_scrapy/spiders/quotes_spider.py
+++ quotes_scrapy/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import hashlib
 
 
 class QuotesSpider(scrapy.Spider):
@@ -16,12 +17,14 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.quote'):
             QuotesSpider.count += 1
+            text = quote.css("span.text::text").get()
             yield {
-                'text': quote.css("span.text::text").get(),
+                "hash": hashlib.md5(text.encode('utf-8')).hexdigest(),
+                'text': text,
                 'author': quote.css("small.author::text").get(),
                 "tags": quote.css("div.tags a.tag::text").getall(),
             }
 
-        yield from response.follow_all(css="li.next a", callback=self.parse)
+        yield from response.follow_all(css="ul.pager a", callback=self.parse)
 
         self.log(f"count: {QuotesSpider.count}")
