78fb86895deca1df742468c6e4834971cfca64c2
vo0doo <kirsanov.bvt@gmail.com>
2022-10-20 13:18:12 +0300

    update settings.py

--- quotes_scrapy/settings.py
+++ quotes_scrapy/settings.py
@@ -62,9 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-# ITEM_PIPELINES = {
-#    'quotes_scrapy.pipelines.Quotes_scrapyPipeline': 300,
-# }
+ITEM_PIPELINES = {
+    'quotes_scrapy.pipelines.DublicatesPipeLine': 100,
+    'quotes_scrapy.pipelines.JsonWriterPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
